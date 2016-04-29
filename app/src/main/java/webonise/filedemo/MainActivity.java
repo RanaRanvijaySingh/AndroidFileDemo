ab374c41cd9f83411968cd2906320e7e44f9050a
Rana Ranvijay Singh <rana.ranvijay@weboniselab.com>
2016-04-29 17:13:43 +0530

    Working for nexus5, htc, lenovo and tablet

---
+++ app/src/main/java/webonise/filedemo/MainActivity.java
@@ -0,0 +1,106 @@
+package webonise.filedemo;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents() {
+        etMessage = (EditText) findViewById(R.id.etMessage);
+    }
+
+    public void onClickWriteButton(View view) {
+        if (isExternalStorageAvailable()) {
+            File file = getFile();
+            String message = etMessage.getText().toString();
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write(message.getBytes());
+                fileOutputStream.close();
+                etMessage.setText("");
+                Toast.makeText(MainActivity.this, "Message saved",
+                        Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "External storage is not available",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @NonNull
+    private File getFile() {
+        String directoryName = "/fileDemo";
+        String fileName = "message.txt";
+        File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File directory = new File(root.getAbsolutePath() + directoryName);
+        if (!directory.exists()) {
+            directory.mkdir();
+            Toast.makeText(MainActivity.this, "Created new directory",
+                    Toast.LENGTH_SHORT).show();
+        }
+        return new File(Environment.getExternalStoragePublicDirectory(Environment
+                .DIRECTORY_DOWNLOADS).getAbsolutePath() + directoryName + fileName);
+    }
+
+    /**
+     * Function is called when user click on read button
+     * @param view View
+     */
+    public void onClickReadButton(View view) {
+        File file = getFile();
+        String message;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((message=bufferedReader.readLine())!=null){
+                stringBuffer.append(message+"\n");
+            }
+            etMessage.setText(stringBuffer.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Checks if external storage is available for read and write
+     *
+     * @return boolean
+     */
+    public boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+}
